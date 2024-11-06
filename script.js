1e22e753c39abbc8437fabb7580cf27d8a14a374
sanat <sanatjha4@gmail.com>
2024-11-06 19:04:59 +0530

    first commit

--- script.js
+++ script.js
@@ -1,35 +1,80 @@
-var textContent = document.getElementsByClassName("text-content")[0].innerHTML;
-var textWordList = textContent.split(" ");
+var textContentElement = document.getElementsByClassName("text-content")[0];
 var typingBox = document.getElementById("typing-box");
 var timer = document.getElementById("timer");
 var wordCount = 0;
 var correctWordCount = 0;
 var typingStarted = false;
 var currentWordN = 0;
-typingBox.addEventListener("input",(element,p)=>{
-    if(!typingStarted){
-        startTimer();
-        typingStarted = true;
+var textWordList = [];
+var boolList = [];
+// import { wordList } from "./wordList.js";
+// Sample list of 1000 common English words
+
+
+// Generate random text by selecting random words from the word list
+function generateRandomText(wordCount = 100) {
+    textWordList = [];
+    for (let i = 0; i < wordCount; i++) {
+        let randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+        textWordList.push(randomWord);
     }
-    else if (element.data == " "){
-        let typedword = typingBox.value;
-        typedword = typedword.slice(0,typedword.length-1);
-        typingBox.value = "";
-        wordCount++;
-        if (typedword==textWordList[currentWordN]) correctWordCount++;
-        currentWordN++;
-    }
-});
-function startTimer(){
+    // let     randomText =  randomTextArray.join(" ");
+    // textContentElement.innerText = randomText;
+    // textWordList = randomText.split(" ");
+    textRefresh();
+}
+
+
+function textRefresh() {
+    textContentElement.innerText = "";
+    console.log(currentWordN);
+    textWordList.forEach((element, index) => {
+        if(index == currentWordN) {
+            textContentElement.innerHTML += `<span class="current-word">${element} </span>`;
+        } else  if (boolList[index]) {
+            textContentElement.innerHTML += `<span class="correct">${element} </span>`;
+        } else if (boolList[index] == undefined) {
+            textContentElement.innerHTML += `<span>${element} </span>`;
+        } else {
+            textContentElement.innerHTML += `<span class="incorrect">${element} </span>`;
+        }
+    })
+}
+
+// Start the typing test timer and event listeners after the text is loaded
+function startTypingTest() {
+    typingBox.addEventListener("input", (element) => {
+        if (!typingStarted) {
+            startTimer();
+            typingStarted = true;
+        } else if (element.data === " " && typingBox.value != " ") {
+            let typedWord = typingBox.value.trim();
+            typingBox.value = "";
+            wordCount++;
+            if (typedWord === textWordList[currentWordN]) correctWordCount++;
+            boolList.push(typedWord === textWordList[currentWordN]);
+            currentWordN++;
+            textRefresh();
+        }
+    });
+}
+
+// Timer function
+function startTimer() {
     timer.innerText = 59;
-    a = setInterval(()=>{
-        timer.innerText = timer.innerText -1;
-        if (timer.innerText == "0"){
+    var a = setInterval(() => {
+        timer.innerText = timer.innerText - 1;
+        if (timer.innerText == "0") {
             typingBox.style.display = "none";
             let resultbox = document.getElementsByClassName("result")[0];
+            resultbox.style.display = "block";
             resultbox.innerText = `Total Words typed: ${wordCount} \nTotal Correct Words typed: ${correctWordCount}`;
             clearInterval(a);
-            return;
         }
-    },1000);
+    }, 1000);
 }
+
+// Initialize the app by setting random text and setting up the typing test
+generateRandomText();
+startTypingTest();
+
